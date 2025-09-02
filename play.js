d63aaa126eea72de232651044a8a67d029d41bab
Adi <devflutter752@gmail.com>
2025-09-02 12:48:25 +0530

    s

--- play.js
+++ play.js
@@ -7,6 +7,10 @@ const bodyParser = require("body-parser");
 const routesPerson = require("./routes/personRoutes");
 const PORT = process.env.PORT || 9142;
 
+app.get("/", function (req, res) {
+  res.send("Welcome to our Projects");
+});
+
 app.use(bodyParser.json());
 app.use("/person", routesPerson);
 
